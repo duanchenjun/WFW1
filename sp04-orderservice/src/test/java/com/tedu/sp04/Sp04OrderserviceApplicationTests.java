f3fcfee2ccdd85d4b9c2d054d6c60bca30f7af0f
Administrator <Administrator@tarena>
2019-11-01 12:25:08 +0800

    4

---
+++ sp04-orderservice/src/test/java/com/tedu/sp04/Sp04OrderserviceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tedu.sp04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Sp04OrderserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		
+	}
+
+}
